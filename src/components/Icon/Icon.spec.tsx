73138a416c9c717e485ff2d8fe7610d7ed12b934
MariaFlorenciaP <auka83@gmail.com>
2023-04-17 19:27:29 -0300

    [COFFEE-06] Creating Icon component

---
+++ src/components/Icon/Icon.spec.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Icon } from "./Icon";
+
+describe("Icon", () => {
+  it('renders the correct icon when `name` is "purpleCart"', () => {
+    render(<Icon name="purpleCart" />);
+    const icon = screen.getByTestId("purpleCart-icon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('renders the correct icon when `name` is "yellowCart"', () => {
+    render(<Icon name="yellowCart" />);
+    const icon = screen.getByTestId("yellowCart-icon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('renders the correct icon when `name` is "plus"', () => {
+    render(<Icon name="plus" />);
+    const icon = screen.getByTestId("plus-icon");
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('renders the correct icon when `name` is "minus"', () => {
+    render(<Icon name="minus" />);
+    const icon = screen.getByTestId("minus-icon");
+    expect(icon).toBeInTheDocument();
+  });
+});
