d450aad84f0b2a72d4650671cdb1ab52e4c8aae9
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-04-21 20:03:30 -0300

    COFFEE-05: LocationInfo refactor

--- src/components/LocationInfo/LocationInfo.tsx
+++ src/components/LocationInfo/LocationInfo.tsx
@@ -1,36 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { ImLocation2 } from 'react-icons/im'
 
-const LocationInfo = () => {
-    const [city, setCity] = useState('')
+export type coordsType = {
+  coords : {
+    latitude: number;
+    longitude: number;
+  }
+}
 
-    useEffect(() => {
-      showCurrentPosition()
-    }, [])
+export const LocationInfo = () => {
+  const [city, setCity] = useState<string>('')
 
-    interface coordsType {
-      latitude: number;
-      longitude: number;
-    }
+  useEffect(() => {
+    showCurrentPosition()
+  }, [])
+
+  const showCurrentPosition = () =>{
+    navigator.geolocation.getCurrentPosition(getCityName)
+  }
 
   async function getCityName(position : coordsType) {
+    const {latitude, longitude } = position.coords
 
-    const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&location_type=RANGE_INTERPOLATED&result_type=street_address&key=${process.env.VITE_API_KEY}`;
+  //   const MOCK_COORDS:  Array<coordsType> = [
+  //     {
+  //         coords: {latitude:-23.9691736, longitude:-46.4088517}
+  //     },
+  //     {
+  //         coords: {latitude:-21.16412174615681, longitude: -47.84836383211025},
+  //     },
+  //     {
+  //         coords: {latitude: -27.60475185902592, longitude: -48.435128382268175}
+  //     },
+  //     {
+  //         coords: {latitude: 43.642472620760216, longitude: -79.38987451164982}
+  //     },
+  // ];
+
+    const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.VITE_API_KEY}`;
+    //const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${MOCK_COORDS[2].coords.latitude},${MOCK_COORDS[2].coords.longitude}&key=${process.env.VITE_API_KEY}`;
 
     const response = await fetch(googleUrl);
     const jsonData = await response.json();
-    const cityName = jsonData.results[0].formatted_address;
+    const cityName = jsonData.results[0]?.formatted_address ?? '';
     console.log('City full name:', cityName)
-    setCity(cityName.split(',')[2])
-  }
-
-
-  const showCurrentPosition = (): void =>{
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
-      getCityName(position.coords)
-    });
+    setCity(cityName?.split(',')[2] ?? 'Somewhere in the world')
   }
 
   return (
