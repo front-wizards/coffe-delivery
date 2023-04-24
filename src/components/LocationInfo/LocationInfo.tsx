4ed804f7bffd9b0706c0564f75ade112c7ad405b
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-04-24 09:18:50 -0300

    COFFEE-05: Update logic to get the city name

--- src/components/LocationInfo/LocationInfo.tsx
+++ src/components/LocationInfo/LocationInfo.tsx
@@ -22,29 +22,29 @@ export const LocationInfo = () => {
   async function getCityName(position : coordsType) {
     const {latitude, longitude } = position.coords
 
-  //   const MOCK_COORDS:  Array<coordsType> = [
-  //     {
-  //         coords: {latitude:-23.9691736, longitude:-46.4088517}
-  //     },
-  //     {
-  //         coords: {latitude:-21.16412174615681, longitude: -47.84836383211025},
-  //     },
-  //     {
-  //         coords: {latitude: -27.60475185902592, longitude: -48.435128382268175}
-  //     },
-  //     {
-  //         coords: {latitude: 43.642472620760216, longitude: -79.38987451164982}
-  //     },
-  // ];
+    // const MOCK_COORDS:  Array<coordsType> = [
+    //   {
+    //       coords: {latitude:-23.9691736, longitude:-46.4088517}
+    //   },
+    //   {
+    //       coords: {latitude:-21.16412174615681, longitude: -47.84836383211025},
+    //   },
+    //   {
+    //       coords: {latitude: -27.60475185902592, longitude: -48.435128382268175}
+    //   },
+    //   {
+    //       coords: {latitude: 43.642472620760216, longitude: -79.38987451164982}
+    //   },
+    // ];
 
     const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.VITE_API_KEY}`;
-    //const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${MOCK_COORDS[2].coords.latitude},${MOCK_COORDS[2].coords.longitude}&key=${process.env.VITE_API_KEY}`;
+    //const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${MOCK_COORDS[3].coords.latitude},${MOCK_COORDS[3].coords.longitude}&key=${process.env.VITE_API_KEY}`;
 
     const response = await fetch(googleUrl);
     const jsonData = await response.json();
     const cityName = jsonData.results[0]?.formatted_address ?? '';
     console.log('City full name:', cityName)
-    setCity(cityName?.split(',')[1] ?? 'Somewhere in the world')
+    setCity(cityName?.split(',').length < 5 ? cityName?.split(',')[1] : cityName?.split(',')[2] ?? 'Somewhere in the world')
   }
 
   return (
