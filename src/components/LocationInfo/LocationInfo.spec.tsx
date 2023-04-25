4649ca1a8329bc2a267a646dd5c1030c45715bf3
Leandro Aparecido Roberto <leandro.roberto@ciandt.com>
2023-04-24 21:41:42 -0300

    COFFEE-05: Update LocationInfo component and add test to it WIP

--- src/components/LocationInfo/LocationInfo.spec.tsx
+++ src/components/LocationInfo/LocationInfo.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
+
 import '@testing-library/jest-dom'
 import { LocationInfo, coordsType } from './LocationInfo'
 
@@ -14,52 +15,51 @@ const MOCK_GOOGLE_RESULTS: Array<googleProps>  = [
     {formatted_address: "1 Blue Jays Way, Toronto, ON M5V 1J4, Canada"},
 ];
 
-const MOCK_COORDS:  Array<coordsType> = [
-    {
-        coords: {latitude:-23.9691736, longitude:-46.4088517}
-    },
-    {
-        coords: {latitude:-21.16412174615681, longitude: -47.84836383211025},
-    },
-    {
-        coords: {latitude: -27.60475185902592, longitude: -48.435128382268175}
-    },
-    {
-        coords: {latitude: 43.642472620760216, longitude: -79.38987451164982}
-    },
-];
-
+const mockGeolocation = {
+    getCurrentPosition: jest.fn()
+        .mockImplementationOnce((success) => Promise.resolve(success({
+            coords: {latitude:-23.9691736, longitude:-46.4088517}
+        })))
+    };
 
 describe('Get the city name based on latitude and longitude', () => {
+
+    beforeEach(() => {
+        global.navigator.geolocation = mockGeolocation;
+      });
+
+      it('Gets Florianópolis as city result when page loads', () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () => Promise.resolve({results : [MOCK_GOOGLE_RESULTS[2]]}),
+            }),
+        ) as jest.Mock;
+
+        const { getByText } = render(<LocationInfo />)
+        expect(getByText('Florianópolis')).toBeInTheDocument()
+    })
+
     it('Gets Ribeirão Preto as city result', () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () => Promise.resolve({results : [MOCK_GOOGLE_RESULTS[1]]}),
+            }),
+        ) as jest.Mock;
 
-        // beforeEach(async () => {
-        //     averagePrice = await getCityName(MOCK_COORDS[1]);
-        // });
-        
-        // global.fetch = jest.fn(() => Promise.resolve({
-        //     json: () => Promise.resolve({result :MOCK_GOOGLE_RESULTS[1]})
-        // }));
+        const { getByText } = render(<LocationInfo />)
+        fireEvent.click(screen.getByTestId('getMyLocation'))
+        expect(getByText('Ribeirão Preto')).toBeInTheDocument()
+    })
 
-        // const { getByText } = render(<LocationInfo />)
-        // expect(getByText('Ribeirão Preto - SP')).toBeInTheDocument()
+    it('Gets Toronto as city result', () => {
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+                json: () => Promise.resolve({results : [MOCK_GOOGLE_RESULTS[3]]}),
+            }),
+        ) as jest.Mock;
 
-
-        expect(1+1).toBe(2);
+        const { getByText } = render(<LocationInfo />)
+        fireEvent.click(screen.getByTestId('getMyLocation'))
+        expect(getByText('Toronto')).toBeInTheDocument()
     })
 })
-
-
-// describe('Gold prices', () => {
-//     let averagePrice;
-
-//     describe('When the average price is called for 7 days', () => {
-//         beforeEach(async () => {
-//             averagePrice = await getPricesLastDays(7);
-//         });
-
-//         it('Then the correct average should be returned', () => {
-//             expect(averagePrice).toEqual({ average: MOCK_AVERAGE });
-//         });
-//     });
-// });
--- src/components/LocationInfo/LocationInfo.tsx
+++ src/components/LocationInfo/LocationInfo.tsx
@@ -22,20 +22,20 @@ export const LocationInfo = () => {
   async function getCityName(position : coordsType) {
     const {latitude, longitude } = position.coords
 
-    // const MOCK_COORDS:  Array<coordsType> = [
-    //   {
-    //       coords: {latitude:-23.9691736, longitude:-46.4088517}
-    //   },
-    //   {
-    //       coords: {latitude:-21.16412174615681, longitude: -47.84836383211025},
-    //   },
-    //   {
-    //       coords: {latitude: -27.60475185902592, longitude: -48.435128382268175}
-    //   },
-    //   {
-    //       coords: {latitude: 43.642472620760216, longitude: -79.38987451164982}
-    //   },
-    // ];
+    /*const MOCK_COORDS:  Array<coordsType> = [
+      {
+          coords: {latitude:-23.9691736, longitude:-46.4088517}
+      },
+      {
+          coords: {latitude:-21.16412174615681, longitude: -47.84836383211025},
+      },
+      {
+          coords: {latitude: -27.60475185902592, longitude: -48.435128382268175}
+      },
+      {
+          coords: {latitude: 43.642472620760216, longitude: -79.38987451164982}
+      },
+    ];*/
 
     const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.VITE_API_KEY}`;
     //const googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${MOCK_COORDS[3].coords.latitude},${MOCK_COORDS[3].coords.longitude}&key=${process.env.VITE_API_KEY}`;
@@ -44,6 +44,7 @@ export const LocationInfo = () => {
     const jsonData = await response.json();
     const cityName = jsonData.results[0]?.formatted_address ?? '';
     console.log('City full name:', cityName)
+    console.log(cityName?.split(',').length < 5 ? cityName?.split(',')[1] : cityName?.split(',')[2] ?? 'Somewhere in the world')
     setCity(cityName?.split(',').length < 5 ? cityName?.split(',')[1] : cityName?.split(',')[2] ?? 'Somewhere in the world')
   }
 
@@ -52,7 +53,7 @@ export const LocationInfo = () => {
         <div>
             <ImLocation2 />
         </div>
-        <div onClick={showCurrentPosition}>
+        <div onClick={showCurrentPosition} data-testid="getMyLocation">
             {city}
         </div>
     </div>
